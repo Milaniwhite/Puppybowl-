0f9eabb4e8ac4a3dd05bedef795f8c4c753390d3
Milani <milaniwhite12@gmail.com>
2025-01-30 17:16:26 -0500

    changes

--- Components/Form.jsx
+++ Components/Form.jsx
@@ -1,11 +1,11 @@
 import { Input } from 'postcss';
-import React from 'react'
+import React , {useState}from 'react'
 import { addPlayer } from './API/Index';
 const Form = () => {
+    const [name, setName] = useState("");
+    const [breed, setBreed] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
     async function handleSubmit(e) {
-        const [name, setName] = useState("");
-        const [breed, setBreed] = useState("");
-        const [imageUrl, setImageUrl] = useState("");
         e.preventDefault();
         const result = await addPlayer(name,breed,imageUrl);
         if (result && result.success) {
@@ -26,8 +26,8 @@ const Form = () => {
         </label>
         <label>
             Breed:
-            <input type="text" value={breed} onChange={(e) => setBreed(e.target.value)}/>
         </label>
+            <input type="text" value={breed} onChange={(e) => setBreed(e.target.value)}/>
         <label>
             Image:
             <input type="text"value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
